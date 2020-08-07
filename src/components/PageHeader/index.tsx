020c4a5c773614e6ebafc3eb4b8d9c889c69f9a5
Ricardo de Carvalho <skapitao@gmail.com>
2020-08-07 19:10:24 -0300

    feat: Finish nlw base

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Text, Image } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -10,9 +10,11 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
     title: string;
+    children?: ReactNode;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
     const { navigate } = useNavigation();
 
     function handleGoBack() {
@@ -27,7 +29,11 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                 </BorderlessButton>
                 <Image source={logoImg} resizeMode='contain' />
             </View>
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+            {children}
         </View>
     )
 }
